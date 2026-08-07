626926a60bda87fbd5be8cd3983d604f1e2a8e7f
Ayush Chaudhary <chaudharyayush7409@gmail.com>
2026-08-07 09:32:04 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,23 @@ require("dotenv").config();
 
 const app = require("./src/index");
 const connectDB = require("./src/config/db");
-connectDB();
-
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
-});
+
+async function startServer() {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+      console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+    process.exit(1);
+  }
+}
+
+startServer();
 
 
