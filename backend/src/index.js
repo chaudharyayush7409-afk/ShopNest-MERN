f7fa8c7a8a4673b30058a10a4aa4f3a0250e7e55
Ayush Chaudhary <chaudharyayush7409@gmail.com>
2026-08-11 12:11:58 +0530

    url c

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://shop-nest-mern-react-gilt.vercel.app",
     credentials: true,
   })
 );
